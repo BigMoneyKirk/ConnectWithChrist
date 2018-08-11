b3cafd31dd02b36427dddfe4608e1442ca233013
Stephen Kirkland <skirkland.job@gmail.com>
2018-08-11 15:46:31 -0400

    Created my solution in VS 17 this time :)

---
+++ ConnectWithChrist.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ConnectWithChrist.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ConnectWithChrist.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ConnectWithChrist.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
