fa8ab3c3d2e583ff4adcfe233a82f5a0b0cbb29b
Stephen Kirkland <skirkland.job@gmail.com>
2018-08-11 16:54:44 -0400

    pushing with the terminal now. goodbye for now, SourceTree

--- ConnectWithChrist.DAL/Entities/User.cs
+++ ConnectWithChrist.DAL/Entities/User.cs
@@ -1,4 +1,5 @@
-﻿using ConnectWithChrist.DAL.Infrastructure;
+﻿using ConnectWithChrist.DAL.Enums;
+using ConnectWithChrist.DAL.Infrastructure;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -26,7 +27,7 @@ namespace ConnectWithChrist.DAL.Entities
 
         public string LastName { get; set; }
 
-        public int UserTypeEnum { get; set; }
+        public UserType UserTypeEnum { get; set; }
 
         public string PrimaryEmail { get; set; }
 
